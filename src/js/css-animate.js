8c52f60c2dbd726039838168e5143628542a1d46
plugins <1051129568@qq.com>
2017-09-13 20:55:56 +0800

    优化整理了代码实现

--- src/js/css-animate.js
+++ src/js/css-animate.js
@@ -8,7 +8,7 @@
 		 * entering:'',
 		 * leave:'animate',
 		 * leaving:'',
-		 * mode:'ease|ease-out|ease-in|ease-in-out'
+		 * 
 		 * }
 		 */
 		$.fn.cssAnimate=animate;
@@ -20,39 +20,29 @@
 	
 	return {
 		enter:function(){
-			var timeout = setTimeout(function(){
-				options.hook.beforeEnter&&options.hook.beforeEnter($self);	
-			},100);
-			
-			$self.addClass(options.enter);
-			var startCall=function(){
-					options.hook.startEnter&&options.hook.startEnter($self);
-					this.removeEventListener('webkitAnimationStart',startCall,false);
-					this.removeEventListener('animationStart',startCall,false);
-			};
-			$self[0].addEventListener&&$self[0].addEventListener('webkitAnimationStart',startCall,false);
-			$self[0].addEventListener&&$self[0].addEventListener('animationStart',startCall,false);
-			var self=this;
-			
 			var enterDone=function(){
 				$self.removeClass(options.enter).removeClass(options.entering);
 				options.hook.afterEnter&&options.hook.afterEnter($self);
-				$self[0].addEventListener&&$self[0].removeEventListener('webkitAnimationEnd',enterCall,false);
-				$self[0].addEventListener&&$self[0].removeEventListener('animationEnd',enterCall,false);
+				$self[0].addEventListener&&$self[0].removeEventListener('webkitAnimationEnd',enterDone,false);
+				$self[0].addEventListener&&$self[0].removeEventListener('animationEnd',enterDone,false);
 			};
 			
-			var enterCall=function(){
-				done=true;
-				clearTimeout(timeout);
+			var timeout = setTimeout(function(){
 				enterDone();
+			},100);
+			options.hook.beforeEnter&&options.hook.beforeEnter($self);
+			$self.addClass(options.enter);
+			var startCall=function(){
+				clearTimeout(timeout);
+				options.hook.startEnter&&options.hook.startEnter($self);
+				this.removeEventListener('webkitAnimationStart',startCall,false);
+				this.removeEventListener('animationStart',startCall,false);
 			};
+			$self[0].addEventListener&&$self[0].addEventListener('webkitAnimationStart',startCall,false);
+			$self[0].addEventListener&&$self[0].addEventListener('animationStart',startCall,false);
 			
-			
-			
-			
-			
-			$self[0].addEventListener&&$self[0].addEventListener('webkitAnimationEnd',enterCall,false);
-			$self[0].addEventListener&&$self[0].addEventListener('animationEnd',enterCall,false);
+			$self[0].addEventListener&&$self[0].addEventListener('webkitAnimationEnd',enterDone,false);
+			$self[0].addEventListener&&$self[0].addEventListener('animationEnd',enterDone,false);
 			//setTimeout(function(){
 				$self.addClass(options.entering);
 			//},16);
@@ -60,10 +50,18 @@
 		},
 		
 		leave:function(){
-			var timeout = setTimeout(function(){
-				options.hook.beforeLeave&&options.hook.beforeLeave($self);	
-			},100);
 			
+			var leaveDone=function(){
+				options.hook.afterLeave&&options.hook.afterLeave($self);
+				$self.removeClass(options.leave).removeClass(options.leaving);
+				$self[0].addEventListener&&$self[0].removeEventListener('webkitAnimationEnd',leaveDone,false);
+				$self[0].addEventListener&&$self[0].removeEventListener('animationEnd',leaveDone,false);
+			};
+			
+			var timeout = setTimeout(function(){
+				leaveDone();
+			},100);
+			options.hook.beforeLeave&&options.hook.beforeLeave($self);
 			var startCall=function(){
 					clearTimeout(timeout);
 					options.hook.startLeave&&options.hook.startLeave($self);
@@ -77,26 +75,9 @@
 			//setTimeout(function(){
 				$self.addClass(options.leaving);
 			//},16);
-			var self=this;
-			
-			
-			var leaveDone=function(){
-				options.hook.afterLeave&&options.hook.afterLeave($self);
-				$self.removeClass(options.leave).removeClass(options.leaving);
-				$self[0].addEventListener&&$self[0].removeEventListener('webkitAnimationEnd',leaveCall,false);
-				$self[0].addEventListener&&$self[0].removeEventListener('animationEnd',leaveCall,false);
-			};
-			
-			var leaveCall=function(){
-				done=true;
-				clearTimeout(timeout);
-				leaveDone();
-			};
 
-
-
-			$self[0].addEventListener&&$self[0].addEventListener('webkitAnimationEnd',leaveCall,false);
-			$self[0].addEventListener&&$self[0].addEventListener('animationEnd',leaveCall,false);
+			$self[0].addEventListener&&$self[0].addEventListener('webkitAnimationEnd',leaveDone,false);
+			$self[0].addEventListener&&$self[0].addEventListener('animationEnd',leaveDone,false);
 			
 		}
 	};
